4d6d40121fec34ec78b6b177cd3ffbfa135e5da3
Sergey Bronnikov <estetus@gmail.com>
2020-01-15 16:18:42 +0300

    Update defaults and path to python in set_sysctl.py

---
+++ scripts/defaults-freebsd.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Set standard production config values that relate to TCP behavior.
+
+# TODO: FreeBSD settings https://github.com/freebsd-net/TCP-IP-Regression-TestSuite#test-plan
+# TODO: https://github.com/freebsd-net/TCP-IP-Regression-TestSuite#ipv4-and-ipv6-protocol-testing
+
+vm.old_mlock = 1
